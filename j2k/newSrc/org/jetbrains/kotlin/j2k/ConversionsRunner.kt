3e7b27763edd2297e5dedffc4c8fce02dfbce472
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-08-07 07:02:40 +0300

    Implement basic default argument converter

--- j2k/newSrc/org/jetbrains/kotlin/j2k/ConversionsRunner.kt
+++ j2k/newSrc/org/jetbrains/kotlin/j2k/ConversionsRunner.kt
@@ -24,6 +24,7 @@ object ConversionsRunner {
     private fun createConversions(context: ConversionContext) = listOf(
         ModalityConversion(context),
         ImplicitInitializerConversion(),
+        DefaultArgumentsConversion(),
         TypeMappingConversion(context),
         FieldToPropertyConversion(),
         AssignmentAsExpressionToAlsoConversion(context),
